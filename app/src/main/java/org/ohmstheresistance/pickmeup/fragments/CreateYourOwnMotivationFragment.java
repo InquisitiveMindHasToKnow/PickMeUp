a2d64e5d5c342a4928a7a1b5fd916247e49b7d9a
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-11-13 21:41:33 -0500

    Added a rv to display additional quotes on display fragment. Modified textsize

--- app/src/main/java/org/ohmstheresistance/pickmeup/fragments/CreateYourOwnMotivationFragment.java
+++ app/src/main/java/org/ohmstheresistance/pickmeup/fragments/CreateYourOwnMotivationFragment.java
@@ -1,38 +1,30 @@
 package org.ohmstheresistance.pickmeup.fragments;
 
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.ohmstheresistance.pickmeup.R;
-
-import java.util.ArrayList;
-
-import static android.app.Activity.RESULT_OK;
 
 
 public class CreateYourOwnMotivationFragment extends Fragment {
 
-    protected static final int RESULT_SPEECH = 1;
-
     private View rootView;
-    private FloatingActionButton speechFab;
-    private EditText speechEditText;
+    private FloatingActionButton createQuoteFab;
+    private EditText quoteEditText;
     private TextView dateMadeTextView;
     private Button saveButton;
+    private CardView createdQuoteCardView;
 
     public CreateYourOwnMotivationFragment() {
         // Required empty public constructor
@@ -45,8 +37,9 @@ public class CreateYourOwnMotivationFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_create_your_own_quote, container, false);
 
-        speechFab = rootView.findViewById(R.id.activate_speech_fab_button);
-        speechEditText = rootView.findViewById(R.id.user_speech_edittext);
+        createdQuoteCardView = rootView.findViewById(R.id.create_quote_cardview);
+        createQuoteFab = rootView.findViewById(R.id.add_quote_fab_button);
+        quoteEditText = rootView.findViewById(R.id.user_speech_edittext);
         saveButton = rootView.findViewById(R.id.save_speech_button);
         dateMadeTextView = rootView.findViewById(R.id.date_quote_was_made);
 
@@ -58,56 +51,19 @@ public class CreateYourOwnMotivationFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
-            dateMadeTextView.setVisibility(View.GONE);
-            saveButton.setVisibility(View.GONE);
+        createdQuoteCardView.setVisibility(View.INVISIBLE);
 
-        speechFab.setOnClickListener(new View.OnClickListener() {
+        createQuoteFab.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                Intent speechToTextIntent = new Intent(
-                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                createdQuoteCardView.setVisibility(View.VISIBLE);
 
-                speechToTextIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-
-                try {
-                    startActivityForResult(speechToTextIntent, RESULT_SPEECH);
-                    speechEditText.setText("");
-
-                } catch (ActivityNotFoundException a) {
-                    Toast t = Toast.makeText(getContext(),
-                            "Opps! Your device doesn't support Speech to Text",
-                            Toast.LENGTH_SHORT);
-                    t.show();
-                }
             }
         });
 
 
-
     }
 
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case RESULT_SPEECH: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> text = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-                    speechEditText.setText(text.get(0));
-                    dateMadeTextView.setVisibility(View.VISIBLE);
-                    saveButton.setVisibility(View.VISIBLE);
-                }
-                break;
-            }
-
-        }
-    }
 }
