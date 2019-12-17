4d32abed035b519a495e7280a4629ebc7d4de257
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-12-16 20:34:30 -0500

    Added more quotes so updated link in QuoteService. Added a snackbar to show a message if no favorites are saved.

--- app/src/main/java/org/ohmstheresistance/pickmeup/fragments/FavoriteMotivationalQuotes.java
+++ app/src/main/java/org/ohmstheresistance/pickmeup/fragments/FavoriteMotivationalQuotes.java
@@ -2,22 +2,28 @@ package org.ohmstheresistance.pickmeup.fragments;
 
 
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.ohmstheresistance.pickmeup.R;
@@ -34,7 +40,6 @@ import static android.view.View.VISIBLE;
 public class FavoriteMotivationalQuotes extends Fragment {
 
 
-
     private View rootView;
     RecyclerView favoriteQuotesRecyclerView;
     private TextView favoriteQuotesHeaderTextView;
@@ -43,6 +48,8 @@ public class FavoriteMotivationalQuotes extends Fragment {
     FavoriteQuotesAdapter favoriteQuotesAdapter;
     private List<Quotes> favoritesQuotesList;
     private MenuItem menuItem;
+
+    private ConstraintLayout favoriteCountriesConstraintLayout;
 
     public FavoriteMotivationalQuotes() {
         // Required empty public constructor
@@ -58,6 +65,8 @@ public class FavoriteMotivationalQuotes extends Fragment {
         favoriteQuotesRecyclerView = rootView.findViewById(R.id.favorite_quotes_recycler_view);
         favoriteQuotesHeaderTextView = rootView.findViewById(R.id.favorite_quotes_header);
         noFavoritesYetHeaderTextView = rootView.findViewById(R.id.no_favorites_yet_header);
+
+        favoriteCountriesConstraintLayout = rootView.findViewById(R.id.favorite_quotes_constraint_layout);
 
         favoriteQuotesAdapter = new FavoriteQuotesAdapter(favoritesQuotesList);
         favoriteQuotesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -81,7 +90,7 @@ public class FavoriteMotivationalQuotes extends Fragment {
 
     }
 
-    ItemTouchHelper.SimpleCallback swipeLeftOrRightToDeleteFavorites = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+    ItemTouchHelper.SimpleCallback swipeLeftOrRightToDeleteFavorites = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
             return false;
@@ -101,12 +110,13 @@ public class FavoriteMotivationalQuotes extends Fragment {
         }
     };
 
-    private void checkWhatHeaderToUse(){
+    private void checkWhatHeaderToUse() {
 
         if (favoritesQuotesList.isEmpty()) {
             favoriteQuotesRecyclerView.setVisibility(GONE);
             noFavoritesYetHeaderTextView.setVisibility(VISIBLE);
             favoriteQuotesHeaderTextView.setVisibility(GONE);
+
         } else {
 
             favoriteQuotesHeaderTextView.setVisibility(VISIBLE);
@@ -117,22 +127,19 @@ public class FavoriteMotivationalQuotes extends Fragment {
     }
 
     @Override
-    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
 
-        if(!favoritesQuotesList.isEmpty()) {
-
-
+        if (!favoritesQuotesList.isEmpty())
             inflater.inflate(R.menu.clear_favorites_menu, menu);
-            menuItem = menu.findItem(R.id.remove_all_favorite_quotes);
-        }
+        menuItem = menu.findItem(R.id.remove_all_favorite_quotes);
     }
 
 
     public boolean onOptionsItemSelected(MenuItem item) {
 
 
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.remove_all_favorite_quotes:
 
                 clearAllFavoriteQuotes();
@@ -142,37 +149,65 @@ public class FavoriteMotivationalQuotes extends Fragment {
         return true;
     }
 
-    private void clearAllFavoriteQuotes(){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext(), R.style.UserInfoDialog);
+    private void clearAllFavoriteQuotes() {
 
-        alertDialog.setTitle("Removing all favorites");
-        alertDialog.setMessage("Are you sure you want to remove all your favorite quotes?: ");
-        alertDialog.setPositiveButton("Yes",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
+        if (favoritesQuotesList.isEmpty()) {
+
+            Snackbar noFavoritesToDeleteSnackbar = Snackbar.make(favoriteCountriesConstraintLayout, "Nothing to delete.", Snackbar.LENGTH_LONG);
+            View snackbarView = noFavoritesToDeleteSnackbar.getView();
+            TextView snackBarTextView = snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+
+            snackBarTextView.setBackgroundResource(R.drawable.custom_snackbar);
+            snackBarTextView.setTextColor(getResources().getColor(R.color.textColor));
+            snackbarView.setBackgroundColor(Color.TRANSPARENT);
+
+            FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) snackbarView.getLayoutParams();
+            layoutParams.width = FrameLayout.LayoutParams.WRAP_CONTENT;
+            layoutParams.gravity = Gravity.CENTER | Gravity.BOTTOM;
+
+            layoutParams.bottomMargin = 200;
+
+            snackbarView.setLayoutParams(layoutParams);
 
 
-                        favoriteQuotesDatabase.clearFavoriteQuotesDatabase();
-                        favoriteQuotesAdapter.setData(favoritesQuotesList);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+                snackBarTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            else
+                snackBarTextView.setGravity(Gravity.CENTER_HORIZONTAL);
 
-                        favoriteQuotesRecyclerView.setVisibility(View.INVISIBLE);
-                        favoriteQuotesHeaderTextView.setVisibility(View.INVISIBLE);
-                        noFavoritesYetHeaderTextView.setVisibility(VISIBLE);
-                        menuItem.setVisible(false);
+            noFavoritesToDeleteSnackbar.show();
+        } else {
 
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext(), R.style.UserInfoDialog);
+
+            alertDialog.setTitle("Removing all favorites");
+            alertDialog.setMessage("Are you sure you want to remove all your favorite quotes?: ");
+            alertDialog.setPositiveButton("Yes",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+
+                            favoriteQuotesDatabase.clearFavoriteQuotesDatabase();
+                            favoriteQuotesAdapter.setData(favoritesQuotesList);
+
+                            favoriteQuotesRecyclerView.setVisibility(View.INVISIBLE);
+                            favoriteQuotesHeaderTextView.setVisibility(View.INVISIBLE);
+                            noFavoritesYetHeaderTextView.setVisibility(VISIBLE);
+                            menuItem.setVisible(false);
 
 
                         }
 
-                });
-        alertDialog.setNegativeButton("No",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
+                    });
+            alertDialog.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                });
-        alertDialog.show();
+                        }
+                    });
+            alertDialog.show();
+        }
+
     }
 
 }
-
